44e76a5d7496ee13fe9540c0bdc7611d9c310dab
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-01-28 00:19:34 -0300

    Update and rename lineage_channel.mk to legion_channel.mk

--- legion_channel.mk
+++ legion_channel.mk
@@ -20,15 +20,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
 
 # Inherit from channel device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+#Device Maintainer
+DEVICE_MAINTAINER := Giovany Veloso
+BUILD_MAINTAINER := Giovany Veloso
+LEGION_BUILD_MAINTAINER := Giovany Veloso
+
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := channel
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_NAME := lineage_channel
+PRODUCT_NAME := legion_channel
 PRODUCT_MODEL := moto g(7) play
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
