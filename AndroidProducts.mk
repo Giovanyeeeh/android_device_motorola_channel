a715c9b9d71c6c497317d05289747a16d26ace08
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-02-07 06:45:29 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_channel.mk
+    $(LOCAL_DIR)/corvus_channel.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_channel-eng \
-    lineage_channel-user \
-    lineage_channel-userdebug
+    corvus_channel-eng \
+    corvus_channel-user \
+    corvus_channel-userdebug
